1cfed92c3d4edea2ff6d48c1a5ca8b3fb2b2fe65
helio tomé <netshark@live.com>
2021-10-11 17:43:38 -0300

    inserção da api

--- script.js
+++ script.js
@@ -5,13 +5,18 @@ document.querySelector('.busca').addEventListener('submit', async(evento) => {
     if (input !== '') {
         showwarning('carregando....')
 
-        let url = `api.openweathermap.org/data/2.5/weather?q={${encodeURI(input)}}&appid={ae131b334db91f967ac6aae89dbbb6fa&units=metric&lang=pt_br}`
+        //let url = `https://api.openweathermap.org/data/2.5/weather?q=RI(input)}}&appid=c5bbb4566082ed254040e5522fc70b99&units=metrics&lang=pt_br`{${encodeU
             // `https://api.openweathermap.org/data/2.5/weather?q={${encodeURI(input)}}&appid={ae131b334db91f967ac6aae89dbbb6fa&units=metric&lang=pt_br}`
 
-        let results = await fetch(url)
+        let results = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=
+        ${encodeURI(input)}&units=metric&lang=pt_br&appid=c5bbb4566082ed254040e5522fc70b99`)
         let json = await results.json()
 
+        
+
         if (json.cod === 200) {
+
+          
 
         } else {
             showwarning('localização não encontrada...')
@@ -21,7 +26,15 @@ document.querySelector('.busca').addEventListener('submit', async(evento) => {
         showwarning('favor digitar a sua busca...')
     }
 })
+function showInfo(){
+    showwarning('')
+    document.querySelector('.resultado').getElementsByClassName.display='block'
+    document.querySelector('.titulo').innerHTML = `${json.name},${json.country}`
+    document.querySelector('.tempInfo').innerHTML=``
+
+}
 
 function showwarning(msg) {
     document.querySelector('.aviso').innerHTML = msg
+
 }
